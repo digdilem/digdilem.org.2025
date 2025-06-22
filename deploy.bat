ec41a65af8c678f41bda500def8ac006ba5daaed
Simon Avery <digdilem@gmail.com>
2025-06-22 21:11:40 +0100

    Updating to reflect development

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,13 @@
 rem deploy.bat for digdilem
 
 c:
-cd \huge\digdilem
+cd \hugo\digdilem
+
+git add .
+git commit -am "Updating to reflect development"
+ git push -f origin master
 
 hugo --cleanDestinationDir
 
-rem export CLOUDFLARE_API_TOKEN="tvXm8ysXzgmx6i4nBHPST0l4B2mqrN7HPz8p4N_E"
-
 npx wrangler pages deploy c:\hugo\digdilem\public --project-name=digdilem25
+
