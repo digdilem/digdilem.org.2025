d48f6c7a9a757ea0eba0f43d67670a56c2b543ac
Simon Avery <digdilem@gmail.com>
2025-11-10 13:56:34 +0000

    Updating to reflect development

--- public/searchindex.en.js
+++ public/searchindex.en.js
@@ -169,7 +169,7 @@ var relearn_searchindex = [
   },
   {
     "breadcrumb": "Digital Dilemma",
-    "content": "Text Adventures\nDuring the late 1980s and early 1990s, I was a part of the thriving British Text Adventure scene and as well as enjoying many such games, I wrote a few too. Many of these were sold through Joan Pancott’s “Witch of Wessex Software” company, some through The Guild Software. Some were released as public domain / freeware. They were written for 8-bit and 16-bit computers such as the Amstrad CPC, and ported to Spectrum and C64, with later ones written for the PC.\nA collection of my Text Adventures for various formats can be found on my github page You can play some these adventures directly from your browser here!\nDanger! Adventurer At Work! Prison Blues TheGerbilRiotOf67 TheMagiciansApprentice TotalRealityDelusion",
+    "content": "Text Adventures\nDuring the late 1980s and early 1990s, I was a part of the thriving British Text Adventure scene and as well as enjoying many such games, I wrote a few too. Many of these were sold through Joan Pancott’s “Witch of Wessex Software” company, some through The Guild Software. Some were released as public domain / freeware. They were written for 8-bit and 16-bit computers such as the Amstrad CPC, and ported to Spectrum and C64, with later ones written for the PC.\nA collection of my Text Adventures for various formats can be found on my github page You can play some these adventures directly from your browser here!\nDanger! Adventurer At Work! Prison Blues The Gerbil Riot of '67 TheMagiciansApprentice TotalRealityDelusion",
     "description": "Text Adventures\nDuring the late 1980s and early 1990s, I was a part of the thriving British Text Adventure scene and as well as enjoying many such games, I wrote a few too. Many of these were sold through Joan Pancott’s “Witch of Wessex Software” company, some through The Guild Software. Some were released as public domain / freeware. They were written for 8-bit and 16-bit computers such as the Amstrad CPC, and ported to Spectrum and C64, with later ones written for the PC.",
     "tags": [],
     "title": "Text Adventures",
@@ -238,14 +238,6 @@ var relearn_searchindex = [
     "tags": [],
     "title": "TheMagiciansApprentice",
     "uri": "/text-adventures/themagiciansapprentice/index.html"
-  },
-  {
-    "breadcrumb": "Digital Dilemma \u003e  Text Adventures",
-    "content": "",
-    "description": "",
-    "tags": [],
-    "title": "TheGerbilRiotOf67",
-    "uri": "/text-adventures/thegerbilriotof67/index.html"
   },
   {
     "breadcrumb": "Digital Dilemma \u003e  Categories",
@@ -349,7 +341,7 @@ var relearn_searchindex = [
     "description": "With thanks to Matt Westcott for JSSpeccy, an open-source ZX Spectrum emulator written in JavaScript\nSolution DANGER: ADVENTURER AT WORK Written by Simon Avery Walk Through by Dorothy Millard (C64 Version)\nYour quest is to find a quiet room where all time stops so you can play adventures as long as you like with no interruptions.\nWalk Through EXAMINE WINDOW, EXAMINE DOOR (locked), GET STOOL, EXAMINE STOOL, THROW STOOL (through the window), EXAMINE WINDOW, CLIMB WINDOW (through window to street), GET HEAD, E, IN (telephone booth), GET ROPE, EXAMINE ROPE, OUT, N, E (field), GET TORSO, EXAMINE TORSO, EXAMINE BULL, TALK BULL, GET BULL (you tie the rope around the ring in his nose), W, S, E, IN (china shop - the shopkeeper drops some boltcutters and runs after the bull), GET BOLTCUTTERS, EXAMINE BOLTCUTTERS, N, W, IN (telephone booth), EXAMINE TELEPHONE, EXAMINE RECEIVER, CUT WIRE (the receiver falls to the floor), GET RECEIVER, EXAMINE RECEIVER, OUT.",
     "tags": [],
     "title": "Danger! Adventurer At Work!",
-    "uri": "/text-adventures/daaw/index.html"
+    "uri": "/text-adventures/dangeradventureratwork/index.html"
   },
   {
     "breadcrumb": "Digital Dilemma \u003e  Sysadmin \u003e  UGreen NAS DXP2800",
@@ -374,6 +366,14 @@ var relearn_searchindex = [
     "tags": [],
     "title": "Category :: Spectrum",
     "uri": "/categories/spectrum/index.html"
+  },
+  {
+    "breadcrumb": "Digital Dilemma \u003e  Text Adventures",
+    "content": "With thanks to Matt Westcott for JSSpeccy, an open-source ZX Spectrum emulator written in JavaScript\nSolution Complete Solution by Duncan Cross (duncan_cross@abelgratis.com)\nStuff in (brackets) is not essential.\nExamine body. Take syringe. South. East. (Go south and watch the film for a hint) North. Take picture. (Examine picture.) South. East. East. North. Take top. Open fridge. Get garlic. North. Get blanket. Wear it. South. South. West. North. (Examine Johnson.) Push north wall. North. Get all. South. South. West. West. West. North. (Examine Oswald.) Get fire bucket. South. West. North. Get the horn. South. South. Show picture to Ralph. South. Get acid. North. North. East. South. Down. Throw bucket on fire. Down. LoIn crates. Get zinc. East. Eat garlic. Examine shadows. Get glands. East. East. East. Pull rope. (Examine plug. Read it.) East. Examine mud. East. (Examine tree. Listen. Talk to tree. Hug tree. Get acorn. Eat it.) West. West. South. [If there is nobody here, you have release 1, you fool. You can still complete the game, however. Those of you with release 2, Blow horn.] (South. Take a leek/leak. Examine it. North.) North. Whistle. South. West. West. West. Up. Up. North. East. North. Unlock cupboard with key. LoIn cupboard. Take copper. Make battery. Tie wires to battery. South. West. South. Down. Down. East. East. East. East. Get rope. East. East. Throw rope. (Climb rope.) Drink monkey glands. Climb rope. In. (Talk to squirrel.) Give battery to squirrel. Out. Down. West. West. West. West. West. West. Up. Up. North. West. North. Spill ink on tissue. Give tissue to shrink. Inject fudge. South. East. East. East. East. South. Give sweetie to Harry. Take jacket. (Wear it.) North. West. West. West. South. Down. Down. East. East. East. South. South. East. Give jacket to Arnold. East. North. Get matches. South. West. West. North. North. North. North. Burn bush. East. East. North. Examine pit. Examine hub. Push button. Give top to Tony. South. South. Show tattoo to crazy people. East. Get wad. West. North. West. West. South. South. West. West. West. Up. Up. North. East. East. East. East. East. Give wad to guard. East.\nCongratulations! You’ve finished Gerbil Riot.\nDisplayed on the Classic Adventures Solution Archive: http://solutionarchive.com",
+    "description": "With thanks to Matt Westcott for JSSpeccy, an open-source ZX Spectrum emulator written in JavaScript\nSolution Complete Solution by Duncan Cross (duncan_cross@abelgratis.com)\nStuff in (brackets) is not essential.\nExamine body. Take syringe. South. East. (Go south and watch the film for a hint) North. Take picture. (Examine picture.) South. East. East. North. Take top. Open fridge. Get garlic. North. Get blanket. Wear it. South. South. West. North. (Examine Johnson.) Push north wall. North. Get all. South. South. West. West. West. North. (Examine Oswald.) Get fire bucket. South. West. North. Get the horn. South. South. Show picture to Ralph. South. Get acid. North. North. East. South. Down. Throw bucket on fire. Down. LoIn crates. Get zinc. East. Eat garlic. Examine shadows. Get glands. East. East. East. Pull rope. (Examine plug. Read it.) East. Examine mud. East. (Examine tree. Listen. Talk to tree. Hug tree. Get acorn. Eat it.) West. West. South. [If there is nobody here, you have release 1, you fool. You can still complete the game, however. Those of you with release 2, Blow horn.] (South. Take a leek/leak. Examine it. North.) North. Whistle. South. West. West. West. Up. Up. North. East. North. Unlock cupboard with key. LoIn cupboard. Take copper. Make battery. Tie wires to battery. South. West. South. Down. Down. East. East. East. East. Get rope. East. East. Throw rope. (Climb rope.) Drink monkey glands. Climb rope. In. (Talk to squirrel.) Give battery to squirrel. Out. Down. West. West. West. West. West. West. Up. Up. North. West. North. Spill ink on tissue. Give tissue to shrink. Inject fudge. South. East. East. East. East. South. Give sweetie to Harry. Take jacket. (Wear it.) North. West. West. West. South. Down. Down. East. East. East. South. South. East. Give jacket to Arnold. East. North. Get matches. South. West. West. North. North. North. North. Burn bush. East. East. North. Examine pit. Examine hub. Push button. Give top to Tony. South. South. Show tattoo to crazy people. East. Get wad. West. North. West. West. South. South. West. West. West. Up. Up. North. East. East. East. East. East. Give wad to guard. East.",
+    "tags": [],
+    "title": "The Gerbil Riot of '67",
+    "uri": "/text-adventures/thegerbilriotof67/index.html"
   },
   {
     "breadcrumb": "Digital Dilemma \u003e  Categories",
